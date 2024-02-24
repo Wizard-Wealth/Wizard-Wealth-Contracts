41bd7d399efddc3c976237178e894ac794d83c13
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2024-02-24 23:20:41 +0700

    Testing for queuing and executing proposal successfully after voting time end

--- contracts/BoxTest.sol
+++ contracts/BoxTest.sol
@@ -6,10 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BoxTest is Ownable {
     uint256 public value;
+
+    event ValueChanged(uint256 newValue);
+
     constructor() Ownable(msg.sender) {}
 
     function store(uint256 _newValue) public onlyOwner {
         value = _newValue;
+        emit ValueChanged(_newValue);
     }
 
     function retrieve() public view returns (uint256) {
